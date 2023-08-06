036570d080d5d85e6aab2227efb4d5d466d4a9ea
Andrés Camilo Yáñez Escobar <camilo_escobar2398@outlook.com>
2023-08-06 09:36:45 -0500

    Modificando el menú de navegación

---
+++ lang/es/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'administration_panel' => [
+        'title' => 'Panel de Administración',
+        'submenu' => [
+            'users' => 'Administración de Usuarios',
+            'product_categories' => 'Administración de Categorías de Productos',
+            'products' => 'Administración de Productos',
+        ]
+    ],
+];
